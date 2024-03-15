3b71983a14771ace0d31c870c198176570ff6f3c
ajaykrupalk <ajaykrupalk@outlook.in>
2024-03-15 23:45:15 +0530

    tested form

--- src/app/components/form-area/form-area.component.ts
+++ src/app/components/form-area/form-area.component.ts
@@ -1,12 +1,11 @@
 import { Component, Output, EventEmitter } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { RequestBody } from '../../interfaces/requestBody';
-import { ParametersComponent } from '../parameters/parameters.component';
 
 @Component({
   selector: 'app-form-area',
   standalone: true,
-  imports: [FormsModule, ParametersComponent],
+  imports: [FormsModule],
   templateUrl: './form-area.component.html',
   styleUrl: './form-area.component.css'
 })
@@ -15,8 +14,10 @@ export class FormAreaComponent {
   @Output() onSubmitData: EventEmitter<RequestBody> = new EventEmitter();
   content!: string;
   prompt: string = '';
-  error: string = '';
   negativePrompt: string = 'cartoon, illustration, animation, face, male, female';
+  inferenceSteps: number = 25;
+  guidanceScale: number = 7.5;
+  error: string = '';
 
   constructor(){
   }
@@ -36,14 +37,25 @@ export class FormAreaComponent {
       return;
     }
 
+    if(this.inferenceSteps < 0 && this.inferenceSteps > 100 || this.guidanceScale < 0 && this.guidanceScale > 20){
+      return;
+    }
+
     const req = {
       content: this.content,
-      prompt: this.prompt
+      prompt: this.prompt,
+      negativePrompt: this.negativePrompt,
+      inferenceSteps: this.inferenceSteps,
+      guidanceScale: this.guidanceScale
     }
 
     this.onSubmitData.emit(req);
 
     this.content = '';
     this.prompt = '';
+    this.negativePrompt = 'cartoon, illustration, animation, face, male, female';
+    this.inferenceSteps = 25;
+    this.guidanceScale = 7.5;
+    this.error = '';
   }
 }
--- src/app/interfaces/requestBody.ts
+++ src/app/interfaces/requestBody.ts
@@ -1,4 +1,7 @@
 export interface RequestBody {
     content: string;
     prompt?: string;
+    negativePrompt?: string;
+    inferenceSteps?: number;
+    guidanceScale?: number
 }
